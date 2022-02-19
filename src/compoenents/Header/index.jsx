515b2d465785fbe812af46bc1ba73c322aa14939
Werton Silva <mr.werton@gmail.com>
2022-02-18 21:00:23 -0300

    started project

---
+++ src/compoenents/Header/index.jsx
@@ -0,0 +1,29 @@
+import { Container, Content, Illustration } from './styles';
+import IllustrationImg from '../../assets/b.png'
+import LogoImg from '../../assets/logoImg.svg'
+
+const Header = () => {
+  return (
+    <Container>
+      <Content>
+        <div>
+        <h1>
+          Toddo
+        </h1>
+        <img src={LogoImg} alt="Logo of app, is a scheduling"/>
+
+        </div>
+        <div class="Description">
+          <p>
+          Um app para organizar suas tarefas
+          </p>
+        </div>
+      </Content>
+      <Illustration>
+          <img src={IllustrationImg} alt="illustration"/>
+      </Illustration>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/themes/index.js
@@ -0,0 +1,7 @@
+export const Colors =  {
+    primary: '#A8ACE5',
+    textDark: '#5C5D67',
+    background: '#EFF4F2',
+    textLight: '#FFFFFF',
+
+}
